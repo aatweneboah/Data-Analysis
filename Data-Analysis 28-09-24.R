6d5125b471b6805f7af6416e56bf12c858eed041
aatweneboah <ktweneboah4@gmail.com>
2024-09-29 21:03:47 +0000

    lab 2 work done

---
+++ Data-Analysis 28-09-24.R
@@ -0,0 +1,7 @@
+#check our current working directory
+getwd()
+
+#set the working directory to different folder
+setwd('//wsl.localhost/Ubuntu/home/aatweneboah/data_analysis/Data-Analysis')
+getwd()
+getwd()
