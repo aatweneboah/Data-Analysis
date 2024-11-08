21a8077ae38ddbc17f70ff1f5a39144e68f06880
aatweneboah <ktweneboah4@gmail.com>
2024-11-08 20:10:49 +0000

    new token

--- Data-Analysis 28-09-24.R
+++
@@ -1,7 +0,0 @@
-#check our current working directory
-getwd()
-
-#set the working directory to different folder
-setwd('//wsl.localhost/Ubuntu/home/aatweneboah/data_analysis/Data-Analysis')
-getwd()
-getwd()
